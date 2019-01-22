73b203aa50e6e5ab71fdbe5ac685225050cd6e4e
Andrew Wood <wood.andrew9@gmail.com>
2019-01-21 19:03:32 -0500

    String function tentatively working (phew!)

--- scanner.cpp
+++ scanner.cpp
@@ -31,12 +31,16 @@ struct cmpByLengthThenByLexOrder {
     }
 };
 
-int cur;    /* current character being processed */
-int peek;   /* next charcter to be processed */
+int cur = 0;    /* current character being processed */
+int peek = 0;   /* next charcter to be processed */
+int line = 0;   /* keeps track of the line number */
 
 int skip(FILE  * inputStream) {
-  /* Potentially needs more updating */
+  /* Potentially needs more updating? */
+  //TODO - check if this needs more updating
   while (isspace(cur)) {
+    if (cur == '\n' || cur == '\r')
+      ++line;
     cur = peek;
     peek = std::fgetc(inputStream);
   }
@@ -44,39 +48,88 @@ int skip(FILE  * inputStream) {
 }
 
 /* Find a token and store the chars into lexeme buffer */
-int scan(char *lexeme, FILE * inputStream)
+int scan(char *lexeme, FILE * inputStream, std::map<const std::string,int, cmpByLengthThenByLexOrder> & tokenMap)
 {
   int i = 0;
+
   /* skip over whitespaces and check for EOF */
   if (skip(inputStream) == EOF)
     return EOF;
-  else if (isalpha(cur) || cur == '_') {
-    // ID TOKEN
-    while (isalpha(cur) || isdigit(cur) || cur == '_') {
+
+  //if a ditto character is encountered
+  else if (cur == '"')
+  {
+    lexeme[i++] = cur;
+    cur = peek;
+    if (peek != EOF)
+      peek = std::fgetc(inputStream);
+    while (isprint(cur) || cur == '\n') //if current character is printable, or if the character is newline
+    {
+      if (cur == '\\')
+      {
+        lexeme[i++] = cur;
+        cur = peek;
+        if (peek != EOF)
+          peek = std::fgetc(inputStream);
+        if (cur == '\"')
+        {
+          lexeme[i++] = cur;
+          cur = peek;
+          if (peek != EOF)
+            peek = std::fgetc(inputStream);
+        }
+      }
+      else if (cur == '\"') //end of string token found
+      {
+        lexeme[i++] = cur;
+        lexeme[i++] = '\0';
+        cur = peek;
+        if (peek != EOF)
+          peek = std::fgetc(inputStream);
+        ++tokenMap["string"];
+        break; //break out of loop and continue
+      }
+      else if (cur == '\n') //in this case, no end quote was found.  print error and increment line number
+      {
+        lexeme[i++] = '\0';
+        std::cout << "missing \" for " << lexeme << " on line " << line++ << std::endl;
+        return 1; //return error code 1
+      }
+      else
+      {
+        lexeme[i++] = cur;
+        cur = peek;
+        if (peek != EOF)
+          peek = std::fgetc(inputStream);
+      }
+    }
+  } //quote case
+
+
+  //Dr. Uh's code begins here
+  else if (isalpha(cur) || cur == '_')
+  {
+    // ident token, keep processing until white space is reached nor non-compliant character is reached
+    while (isalpha(cur) || isdigit(cur) || cur == '_')
+    {
       lexeme[i++] = cur;
       cur = peek;
       if (peek != EOF)
         peek = std::fgetc(inputStream);
-
     }
     lexeme[i] = '\0';
-    return i; // return any value other than EOF
+    return 0; // successful return
   }
   else
   {
-
-
     /*** yada yada ***/
-
-
-
   }
   return 0;
 }
 
 void printSummary (std::map<const std::string,int, cmpByLengthThenByLexOrder> map)
 {
-  printf(" token count\n");
+  printf("        token         count\n");
   printf("--------------------- -----\n");
   printf("%21s %5d\n","lexeme",5);
   map.begin();
@@ -85,17 +138,19 @@ void printSummary (std::map<const std::string,int, cmpByLengthThenByLexOrder> ma
 int main()
 {
   char lexeme[MAXTOK];
+  line = 1; //
   int  result;
 
   //used for debugging purposes with CLion
 
+  //TODO - Change this back to stdin for linprog use
   //FILE inputStream = stdin;
   FILE * inputStream = fopen("input.in","r");
 
-  std::string testString = "number";
-  std::string testToken = "string";
+  //std::string testString = "number";
+  //std::string testToken = "string";
 
-  std::cout << testString.compare(testToken);
+  //std::cout << testString.compare(testToken);
 
 
   //create map to hold counts, initialize all to 0
@@ -134,8 +189,11 @@ int main()
   if (cur != EOF)
     peek = std::fgetc(inputStream);
 
-  while ((result = scan(lexeme, inputStream)) != EOF) {
-    std::cout << lexeme << std::endl;
+
+  while ((result = scan(lexeme, inputStream, tokenMap)) != EOF) //1 indicates error code
+  {
+    if (result != 1)
+      std::cout << lexeme << std::endl;
   }
 
   //prints the summary table
